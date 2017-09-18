7d1d3161fe4d4dc99f6f325c6fb15e15350347f9
Pol <openclassrooms@dupont.io>
2017-09-18 11:47:40 +0200

    Création de de deux scripts en python vides pour le moment

---
+++ coucou.py
@@ -0,0 +1,2 @@
+# Bonjour! ce fichier est vide pour le moment
+# Il s'agira d'un script en python qui dit bonjour :)
---
+++ futurprojet.py
@@ -0,0 +1,3 @@
+#Bonjour! 
+#Ce fichier se construira progressivement, sûrement en 4 commit cela dit :)
+#Il contiendra un script en python permettant de connaître tous les vendredis 13 d'une année donnée.
